6d94959f0f3c2837e13f91fc79ae2a71812218ed
Mona696969 <72520715+Mona696969@users.noreply.github.com>
2021-05-19 23:19:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+
+
+export default class App extends React.Component {
+  render() {
+    return 
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
